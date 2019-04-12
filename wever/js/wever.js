c2ab1187567443611713838efe536799dcf4147e
uehr <uehara.yudai@gmail.com>
2019-04-12 23:15:24 +0900

    fix: large file download fails

--- wever/js/wever.js
+++ wever/js/wever.js
@@ -1,11 +1,16 @@
 $(function () {
   setInterval(() => {
     const parentWidth = parseInt($("#before-after-example").parent().css("width"))
+    const parentHeight = parseInt($("#before-after-example").parent().css("height"))
     $("#before-after-example").css("width", parentWidth)
-    $("#image-preview").css("width", parentWidth * 0.7)
+    $("#image-preview").css("width", parentWidth)
   }, 100)
 
+  $("#download-link").css("display", "none")
+  $("#image-process-ui").hide()
+
   const personMosaicURL = "http://localhost/personMosaic"
+  var selectedFileName = ""
 
   const imageToBase64 = (imgTagId, mimeType) => {
     const img = document.getElementById(imgTagId);
@@ -32,7 +37,7 @@ $(function () {
   }
 
   const showFileUploader = () => {
-    $("#file-upload").hide().fadeIn(2000)
+    $("#file-upload").hide().fadeIn(1000)
     $("#file-select").hide()
   }
 
@@ -41,11 +46,19 @@ $(function () {
     $("#image-preview").attr("src", src)
     $("#upload-image").attr("src", src)
     showFileUploader()
+    $("html,body").animate({ scrollTop: $(document).height() }, 1000);
+    $("#download-link").css("display", "none")
+    $("#image-process-ui").show()
+
+    setTimeout(() => {
+      $("#image-size").text($("#upload-image").height() + " × " + $("#upload-image").width())
+    }, 100)
   }
 
   const showFileSelecter = () => {
     $("#file-upload").hide()
-    $("#file-select").hide().fadeIn(2000)
+    $("#image-process-ui").hide()
+    $("#file-select").hide().fadeIn(1000)
   }
 
   const personMosaic = base64Img => {
@@ -64,6 +77,23 @@ $(function () {
     showFileSelecter()
   })
 
+
+  const dataURIToBlob = (dataURI) => {
+    var binStr = atob(dataURI.split(',')[1]),
+      len = binStr.length,
+      arr = new Uint8Array(len),
+      mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    for (var i = 0; i < len; i++) {
+      arr[i] = binStr.charCodeAt(i);
+    }
+
+    return new Blob([arr], {
+      type: mimeString
+    });
+  }
+
+
   $("#upload-button").click(() => {
     const base64Img = imageToBase64("upload-image", "image/jpg")
 
@@ -72,7 +102,17 @@ $(function () {
         alert("処理に失敗しました")
       } else {
         res.json().then(processed => {
-          $("#image-preview").attr("src", "data:image/jpg;base64," + processed.base64Img)
+          const downloadFileName = selectedFileName.replace(".jpg", "-mosaic.jpg").replace(".jpeg", "-mosaic.jpeg")
+          const src = "data:image/jpg;base64," + processed.base64Img
+          const blob = dataURIToBlob(src);
+          const objUrl = URL.createObjectURL(blob);
+
+          $("#image-preview").attr("src", src)
+          $("#processed-image").attr("src", src)
+          $("#download-link").attr("href", objUrl)
+
+          $("#download-link").attr("download", downloadFileName)
+          $("#download-link").css("display", "inline-block")
         })
       }
     }).catch(error => {
@@ -83,6 +123,7 @@ $(function () {
   $("input[type=file]").change(function () {
     const file = this.files[0]
     const fr = new FileReader()
+    selectedFileName = file.name
 
     if (file == null || !isValidFile(file)) return false
 
