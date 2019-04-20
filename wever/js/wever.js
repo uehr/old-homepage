1bf627c7a524bec09c90e8412bd067a19f079eb2
uehr <uehara.yudai@gmail.com>
2019-04-20 20:55:38 +0900

    update: slow switch mosaic effect

--- wever/js/wever.js
+++ wever/js/wever.js
@@ -1,28 +1,31 @@
 $(function () {
   setInterval(() => {
     const parentWidth = parseInt($("#before-after-example").parent().css("width"))
-    const parentHeight = parseInt($("#before-after-example").parent().css("height"))
 
     $("#before-after-example").css("width", parentWidth)
-    $("#image-preview").css("width", parentWidth)
     $("#upload-ui").css("width", parentWidth)
+
+    $("#upload-image-preview").css("width", parentWidth)
+    $("#processed-image-preview").css("width", parentWidth)
   }, 100)
 
   $("#image-process-ui").hide()
   $("#processing-view").hide()
+  $("#processed-image-preview").hide()
 
-  const personMosaicURL = "https://wever.uehr.co/personMosaic"
+  // const personMosaicURL = "https://wever.uehr.co/personMosaic"
+  const personMosaicURL = "http://localhost/personMosaic"
   var selectedFileName = ""
 
   const imageToBase64 = (imgTagId, mimeType) => {
-    const img = document.getElementById(imgTagId);
+    const img = document.getElementById(imgTagId)
 
-    var canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
+    var canvas = document.createElement('canvas')
+    canvas.width = img.width
+    canvas.height = img.height
 
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0, img.width, img.height);
+    var ctx = canvas.getContext('2d')
+    ctx.drawImage(img, 0, 0, img.width, img.height)
 
     const base64 = canvas.toDataURL(mimeType).split(",")[1]
 
@@ -48,7 +51,7 @@ $(function () {
 
   const showUploadImagePreview = src => {
     $("#file-select-input").attr("value", "")
-    $("#image-preview").attr("src", src)
+    $("#upload-image-preview").attr("src", src)
     $("#upload-image").attr("src", src)
     $("#download-button").hide()
     $("#upload-button").hide().fadeIn(500)
@@ -56,7 +59,7 @@ $(function () {
     showFileUploader()
 
     setTimeout(() => {
-      $("html,body").animate({ scrollTop: $("#upload-ui").offset().top }, 1000);
+      $("html,body").animate({ scrollTop: $("#image-preview-wrap").offset().top }, 1000)
     }, 100)
   }
 
@@ -64,26 +67,30 @@ $(function () {
     $("#file-upload").hide()
     $("#image-process-ui").hide()
     $("#file-select").hide().fadeIn(1000)
+    $("#processed-image-preview").attr("src", "")
+    $("#upload-image-preview").attr("src", "")
   }
 
   const startProcessingView = () => {
     $("#processing-view").show()
-    $("#image-preview").css("opacity", 0.3)
+    $("#upload-image-preview").css("opacity", 0.3)
+    $(".image-preview").css("transition", "3s")
   }
 
   const finishProcessingView = () => {
     $("#processing-view").hide()
-    $("#image-preview").css("opacity", 1)
+    $("#upload-image-preview").css("opacity", 1)
+    $(".image-preview").css("transition", "0s")
   }
 
   const personMosaic = base64Img => {
     const json = { "base64Img": base64Img }
     const body = JSON.stringify(json)
-    const method = "POST";
+    const method = "POST"
     const headers = {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
-    };
+    }
 
     return fetch(personMosaicURL, { method, headers, body })
   }
@@ -92,37 +99,35 @@ $(function () {
     showFileSelecter()
   })
 
-
   const dataURIToBlob = (dataURI) => {
     var binStr = atob(dataURI.split(',')[1]),
       len = binStr.length,
       arr = new Uint8Array(len),
-      mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      mimeString = dataURI.split(',')[0].split(':')[1].split('')[0]
 
-    for (var i = 0; i < len; i++) {
-      arr[i] = binStr.charCodeAt(i);
-    }
+    for (var i = 0; i < len; i++)
+      arr[i] = binStr.charCodeAt(i)
 
     return new Blob([arr], {
       type: mimeString
-    });
+    })
   }
 
   const fileDownload = (src, name) => {
-    const a = document.createElement('a');
-    a.href = src;
-    a.download = name;
+    const a = document.createElement('a')
+    a.href = src
+    a.download = name
 
-    a.style.display = 'none';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    a.style.display = 'none'
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
   }
 
   $("#download-button").click(() => {
     const src = $("#processed-image").attr("src")
-    const blob = dataURIToBlob(src);
-    const objUrl = URL.createObjectURL(blob);
+    const blob = dataURIToBlob(src)
+    const objUrl = URL.createObjectURL(blob)
     const downloadFileName = selectedFileName.replace(".jpg", "-mosaic.jpg").replace(".jpeg", "-mosaic.jpeg").replace(".png", "-mosaic.png")
 
     fileDownload(objUrl, downloadFileName)
@@ -141,14 +146,14 @@ $(function () {
         res.json().then(processed => {
           const src = "data:image/jpg;base64," + processed.base64Img
 
-          $("#image-preview").attr("src", src)
+          $("#processed-image-preview").attr("src", src)
+          $("#processed-image-preview").hide().fadeIn(3000)
+          // $("#processed-image-preview").show()
           $("#processed-image").attr("src", src)
           $("#upload-button").hide()
           $("#download-button").hide().fadeIn(500)
 
-          setTimeout(() => {
-            finishProcessingView()
-          }, 100)
+          finishProcessingView()
         })
       }
     }).catch(error => {
