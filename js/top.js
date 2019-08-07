be547a2301a30b915ef9fecd6dbacc1d54919c90
uehr <uehara.yudai@gmail.com>
2019-08-07 21:43:10 +0900

    update: content size

--- js/top.js
+++ js/top.js
@@ -4,13 +4,13 @@ $(function () {
   $('.headline-wrap').hide().fadeIn(500);
   $('.post').hide().fadeIn(500);
   $('.product-wrap').hide().fadeIn(500);
-  var bottom_margin = '30px';
-  var left_margin = '20px';
+
+  var bottom_margin = '6vh';
+
   if ($(window).width() < 500) {
-    bottom_margin = '10px';
-    left_margin = '20px';
+    bottom_margin = '4vh';
   };
-  $('.content-ele-title').animate({ marginLeft: left_margin }, 1500, 'swing');
+
   $('.headline-wrap').animate({ marginBottom: bottom_margin }, 1000, 'swing');
   $('.product-wrap').animate({ marginBottom: bottom_margin }, 1000, 'swing');
 })
